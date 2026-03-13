9eaf139f7d06ca18f3a81f4a362ada30b60e393c
Gu Zhenhao <zhenhao.gu@u.nus.edu>
2026-03-13 13:55:00 +0800

    Report average error rate

--- src/inference.rs
+++ src/inference.rs
@@ -13,7 +13,8 @@ use rand::Rng;
 pub struct ErrorSpectrum {
     pub estimated_lambda: (f32, (f32, f32)),
     pub estimated_beta: (f32, (f32, f32)),
-    
+    pub average_error_rate: (f32, (f32, f32)),
+
     pub key_coverage: (f32, (f32, f32)),
     pub estimated_coverage: (f32, (f32, f32)),
 
@@ -410,6 +411,25 @@ impl ErrorAnalyzer {
         }
     }
 
+    /**
+     * Estimate 1/E[T], where T~DiscreteWeibull(lambda, beta)
+     * E[T] = sum_{t=1}^{\infty} P(T >= t) = sum_{t=1}^{\infty} exp(-lambda * t^beta)
+     * approximate the sum until the terms are small enough
+     */
+    fn estimate_average_error_rate(&self, lambda: f32, beta: f32) -> f32 {
+        let mut expected_t = 0.0;
+        let epsilon: f32 = 1e-6;
+        let max_iterations: usize = 10000;
+        for t in 1..max_iterations {
+            let survival_prob = (- lambda * (t as f32).powf(beta)).exp();
+            if survival_prob < epsilon {
+                break;
+            }
+            expected_t += survival_prob;
+        }
+        1.0 / expected_t
+    }
+
 
     
 
@@ -539,15 +559,16 @@ impl ErrorAnalyzer {
     }
 
     /**
-     * Returns ((lower_a, upper_a), (lower_b, upper_b), hazard_ratio_list)
+     * Returns ((lower_lambda, upper_lambda), (lower_beta, upper_beta), hazard_ratio_list, (lower_error_rate, upper_error_rate))
      */
-    pub fn estimate_hazard_ratio_confidence_interval(&self, stats: &KVmerStats, indices: &Vec<usize>) -> ((f32, f32), (f32, f32), Vec<(f32, f32) >) {
+    pub fn estimate_hazard_ratio_confidence_interval(&self, stats: &KVmerStats, indices: &Vec<usize>) -> ((f32, f32), (f32, f32), Vec<(f32, f32)>, (f32, f32)) {
         let mut x: &Vec<u32>;
         let mut y: &Vec<u32>;
 
         // record the estimated a and b
         let mut lambda_list: Vec<f32> = Vec::new();
         let mut beta_list: Vec<f32> = Vec::new();
+        let mut error_rate_list: Vec<f32> = Vec::new();
 
         // record hazard ratios for each v
         let mut hazard_ratio_list: Vec<Vec<f32>> = Vec::new();
@@ -578,6 +599,7 @@ impl ErrorAnalyzer {
             let (lambda, beta) = self.fit_hazard_ratio(&hazard_ratios);
             lambda_list.push(lambda);
             beta_list.push(beta);
+            error_rate_list.push(self.estimate_average_error_rate(lambda, beta));
         }
 
         lambda_list.sort_by(f32::total_cmp);
@@ -588,7 +610,7 @@ impl ErrorAnalyzer {
         let lower_beta = beta_list[(self.args.num_experiments as f32 * 0.05) as usize];
         let upper_beta = beta_list[(self.args.num_experiments as f32 * 0.95) as usize];
 
-        let mut hazard_ratio_range_list: Vec<(f32, f32) > = Vec::new();
+        let mut hazard_ratio_range_list: Vec<(f32, f32)> = Vec::new();
         for v in 0..hazard_ratio_list.len() {
             hazard_ratio_list[v].sort_by(f32::total_cmp);
             let h_lower = hazard_ratio_list[v][(self.args.num_experiments as f32 * 0.05) as usize];
@@ -596,7 +618,11 @@ impl ErrorAnalyzer {
             hazard_ratio_range_list.push((h_lower, h_upper));
         }
 
-        ((lower_lambda, upper_lambda), (lower_beta, upper_beta), hazard_ratio_range_list)
+        error_rate_list.sort_by(f32::total_cmp);
+        let lower_error_rate = error_rate_list[(self.args.num_experiments as f32 * 0.05) as usize];
+        let upper_error_rate = error_rate_list[(self.args.num_experiments as f32 * 0.95) as usize];
+
+        ((lower_lambda, upper_lambda), (lower_beta, upper_beta), hazard_ratio_range_list, (lower_error_rate, upper_error_rate))
         /* 
         let mut mean = alpha_list.iter().zip(beta_list.iter())
             .map(|(&a, &b)| a / (a + b))
@@ -795,7 +821,8 @@ impl ErrorAnalyzer {
 
         // estimate hazard ratio parameters
         let (lambda, beta, hazard_ratio, x_sum, y_sum) = self.estimate_hazard_ratio(stats, &indices);
-        let (lambda_ci, beta_ci, hazard_ratio_ci) = self.estimate_hazard_ratio_confidence_interval(stats, &indices);
+        let (lambda_ci, beta_ci, hazard_ratio_ci, error_rate_ci) = self.estimate_hazard_ratio_confidence_interval(stats, &indices);
+        let average_error_rate = self.estimate_average_error_rate(lambda, beta);
 
         if let Some(hazard_ratio_output) = &self.args.hazard_rate {
             use std::fs::File;
@@ -824,6 +851,7 @@ impl ErrorAnalyzer {
         ErrorSpectrum {
             estimated_lambda: (lambda, lambda_ci),
             estimated_beta: (beta, beta_ci),
+            average_error_rate: (average_error_rate, error_rate_ci),
 
             key_coverage: key_coverage,
             estimated_coverage: estimated_coverage,
@@ -844,6 +872,9 @@ pub fn spectrum_to_str(spectrum: &ErrorSpectrum, bidirectional: bool) -> String 
     if bidirectional != spectrum.bidirectional {
         panic!("The bidirectional flag does not match the spectrum data.");
     }
+
+    // average error rate
+    result.push_str(&format!("{:.6},{:.6}~{:.6},", spectrum.average_error_rate.0, (spectrum.average_error_rate.1).0, (spectrum.average_error_rate.1).1));
 
     // hazard ratio parameters a and b
     result.push_str(&format!("{:.6},{:.6}~{:.6},", spectrum.estimated_lambda.0, (spectrum.estimated_lambda.1).0, (spectrum.estimated_lambda.1).1));
@@ -873,6 +904,7 @@ pub fn spectrum_to_str(spectrum: &ErrorSpectrum, bidirectional: bool) -> String 
 pub fn header_str(bidirectional: bool) -> String {
     let mut result = String::new();
 
+    result.push_str("average_error_rate,average_error_rate_5-95th_percentile,");
     result.push_str("lambda,lambda_5-95th_percentile,");
     result.push_str("beta,beta_5-95th_percentile,");
 
